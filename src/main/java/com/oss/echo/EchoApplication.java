b8dd3deeda2c0e1feeeda6f6ec4b2633a4f89618
Praveen Manthena <pmanthena@mgmresorts.com>
2020-07-25 08:29:45 -0700

    update gitignore

--- src/main/java/com/oss/echo/EchoApplication.java
+++ src/main/java/com/oss/echo/EchoApplication.java
@@ -1,4 +1,4 @@
-package com.bbtutorials.echo;
+package com.oss.echo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/oss/echo/EchoController.java
+++ src/main/java/com/oss/echo/EchoController.java
@@ -1,4 +1,4 @@
-package com.bbtutorials.echo;
+package com.oss.echo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
