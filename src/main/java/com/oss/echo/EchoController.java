db3e935cabbf90ea5e597cb95b32f82692468593
Praveen Manthena <pmanthena@mgmresorts.com>
2020-07-25 14:36:12 -0700

    controller changes

--- src/main/java/com/oss/echo/EchoController.java
+++ src/main/java/com/oss/echo/EchoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oss.common.OrderStatus;
+import com.oss.domain.CustomerOrderItemResponse;
 import com.oss.domain.CustomerOrderResponse;
 import com.oss.echo.model.Order;
 import com.oss.echo.model.OrderItem;
@@ -24,14 +26,43 @@ public class EchoController {
 	@GetMapping("/orders/{orderId}")
     public CustomerOrderResponse getOrders(@PathVariable String orderId) {
 		CustomerOrderResponse ret = new CustomerOrderResponse();
+		ret.setChannel("Web");
+		ret.setCustomerName("Bob");
+		ret.setOrderStatus(OrderStatus.WIP);
+		ret.setTotalPrice(255.23);
+		List<CustomerOrderItemResponse> a= new ArrayList<CustomerOrderItemResponse>();
+		
+		
+		CustomerOrderItemResponse item = new CustomerOrderItemResponse();
+		item.setDescription("Burger");
+		item.setStatus(OrderStatus.WIP);
+		a.add(item);
+		ret.setOrderItems(a);
 		
         return ret ;
     }
 	
 	@GetMapping("/workstations/{wid}/items")
-    public List<OrderItem> getWorkstationBacklog(@PathVariable String wid) {
-		List<OrderItem> ret = new ArrayList<OrderItem>();
-        return ret;
+    public List<CustomerOrderItemResponse> getWorkstationBacklog(@PathVariable String wid) {
+		List<CustomerOrderItemResponse> a = new ArrayList<CustomerOrderItemResponse>();
+		CustomerOrderItemResponse item = new CustomerOrderItemResponse();
+		item.setDescription("Burger");
+		item.setStatus(OrderStatus.ORDER_RECEIVED);
+		
+		CustomerOrderItemResponse item1 = new CustomerOrderItemResponse();
+		item.setDescription("Pizza");
+		item.setStatus(OrderStatus.WIP);
+		
+		CustomerOrderItemResponse item2 = new CustomerOrderItemResponse();
+		item.setDescription("Sandwich");
+		item.setStatus(OrderStatus.ORDER_RECEIVED);
+		
+		a.add(item);
+		a.add(item1);
+		a.add(item2);
+		
+
+        return a;
     }
 
 }
