7ca29643d12a302facfee00e055da20a32a6c9dc
Praveen Manthena <pmanthena@mgmresorts.com>
2020-07-25 13:30:15 -0700

    wip

--- src/main/java/com/oss/echo/EchoController.java
+++ src/main/java/com/oss/echo/EchoController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import hcl.model.Order;
-import hcl.model.OrderItem;
+import com.oss.echo.model.Order;
+import com.oss.echo.model.OrderItem;
+
+
 
 @RestController
 public class EchoController {
